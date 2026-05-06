c837aed1d8b484f8e4bfe8b9d80de70f9b868595
eduardoblaze <emilgorki@gmail>
2026-05-06 18:35:09 -0500

    #change number 2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int guessCount = 0;
+
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); // 1 to 1'00 inclusive
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            // (stretch challenge)
+            guessCount++;
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        Console.WriteLine($"It took you {guessCount} guess(es).");
     }
 }
